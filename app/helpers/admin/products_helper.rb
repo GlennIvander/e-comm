5cef317427451e4785ed94e110bb4cbbcb8d9819
GlennIvander <ivander792@gmail.com>
2025-06-09 16:47:11 +0800

    dashboard top 4, 2:06

--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -1,5 +1,2 @@
 module Admin::ProductsHelper
-  def number_with_delimiter(number, options = {})
-    delegate_number_helper_method(:number_to_delimited, number, options)
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def number_with_delimiter(number, options = {})
+    delegate_number_helper_method(:number_to_delimited, number, options)
+  end
 end
