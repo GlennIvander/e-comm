d73e089401c15c25498ca1f763c96e0ed5e8c47b
GlennIvander <ivander792@gmail.com>
2025-06-14 15:32:12 +0800

    category show page, 3:05

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ApplicationRecord
-  # has_many_attached :images do |attachable|
-  #   attachable.variant :thumb, resize_to_limit: [ 50, 50 ]
-  # end
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize_to_limit: [ 50, 50 ]
+    attachable.variant :medium, resize_to_limit: [ 250, 250 ]
+  end
 
-  has_one_attached :images
+  # has_one_attached :images
 
   belongs_to :category
   has_many :stocks, dependent: :destroy
